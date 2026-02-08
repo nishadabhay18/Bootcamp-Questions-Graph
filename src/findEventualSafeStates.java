c355549f5b375c27d7a6b0756381c17218336b42
nishadabhay18 <abhayn927@gmail.com>
2026-02-08 19:43:46 +0530

    init commit

---
+++ src/findEventualSafeStates.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class SolufindEventualSafeStatestion {
+    // Reverse Graph Edges
+    public static List<Integer> eventualSafeNodes(int[][] graph) {
+        int n=graph.length;
+        List<Integer> ans=new ArrayList<>();
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        int[] indegree=new int[n];
+        for(int i=0;i<n;i++){
+            for(int edge:graph[i]){
+                adj.get(edge).add(i);
+                indegree[i]++;
+            }
+        }
+        // Kahn's Algorithm
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(q.size()!=0){
+            int size=q.size();
+            int val=q.remove();
+            ans.add(val);
+            for(int ele:adj.get(val)){
+                indegree[ele]--;
+                if(indegree[ele]==0){
+                    q.add(ele);
+                }
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] graph={{1,2},{2,3},{5},{0},{5},{},{}};
+        List<Integer> ans=eventualSafeNodes(graph);
+        for(int i=0;i<ans.size();i++){
+            System.out.println(ans.get(i)+" ");
+        }
+    }
+}
