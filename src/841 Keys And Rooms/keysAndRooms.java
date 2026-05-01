db69a32ac1bdd52af0385e82ce4b86a6d33a433f
nishadabhay18 <abhayn927@gmail.com>
2026-05-01 22:42:37 +0530

    Refactor variable formatting and spacing in keysAndRooms

--- src/841 Keys And Rooms/keysAndRooms.java
+++ src/841 Keys And Rooms/keysAndRooms.java
@@ -1,25 +1,25 @@
 class keysAndRooms {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int n=rooms.size();
-        boolean []isVisited=new boolean[n];
-        isVisited[0]=true;
-        bfs(rooms,isVisited);
-        for(boolean val:isVisited){
-            if(val==false) return false;
+        int n = rooms.size();
+        boolean []isVisited = new boolean[n];
+        isVisited[0] = true;
+        bfs(rooms, isVisited);
+        for(boolean val : isVisited){
+            if(val == false) return false;
         }
         return true;
     }
     public void bfs(List<List<Integer>> rooms, boolean []isVisited){
-        int n=rooms.size();
-        Queue<Integer> q=new LinkedList<>();
+        int n = rooms.size();
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        isVisited[0]=true;
-        while(q.size()!=0){
-            int front=q.remove();
-            for(int ele:rooms.get(front)){
-                if(isVisited[ele]==false){
+        isVisited[0] = true;
+        while(q.size() != 0){
+            int front = q.remove();
+            for(int ele : rooms.get(front)){
+                if(isVisited[ele] == false){
                     q.add(ele);
-                    isVisited[ele]=true;
+                    isVisited[ele] = true;
                 }
             }
         }
