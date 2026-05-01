9b5c0a992606224c9090e0cf0a396c0479c54fb1
nishadabhay18 <abhayn927@gmail.com>
2026-05-01 22:29:00 +0530

    Update courseSchedule.java

--- src/207 Course Schedule/courseSchedule.java
+++ src/207 Course Schedule/courseSchedule.java
@@ -3,29 +3,27 @@ class courseSchedule {
     // subject[3,2].
     // if want to finish subject 3 first complete subject 2 so we gofrom 2 to 3.
     public boolean canFinish(int n, int[][] pre) {
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        int[] indegree=new int[n];
-        for(int i=0;i<pre.length;i++){
-            int a=pre[i][0], b=pre[i][1];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
+        int[] indegree = new int[n];
+        for(int i = 0; i<pre.length; i++){
+            int a = pre[i][0], b = pre[i][1];
             adj.get(b).add(a);
             indegree[a]++;
         }
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.add(i);
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            if(indegree[i] == 0) q.add(i);
         }
-        while(q.size()!=0){
-            int val=q.remove();
+        while(q.size() != 0){
+            int val = q.remove();
             ans.add(val);
-            for(int ele:adj.get(val)){
+            for(int ele : adj.get(val)){
                 indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.add(ele);
-                }
+                if(indegree[ele] == 0) q.add(ele);
             }
         }
-        return (ans.size()==n)? true:false;
+        return (ans.size() == n) ? true : false;
     }
 }
