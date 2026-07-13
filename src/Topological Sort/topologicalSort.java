bf1d8609a1c30839cb6d987c72c76839a248f618
Abhay Kumar Nishad <abhayn927@gmail.com>
2026-07-13 20:23:56 +0530

    Update topologicalSort.java

--- src/Topological Sort/topologicalSort.java
+++ src/Topological Sort/topologicalSort.java
@@ -1,45 +1,49 @@
-class topologicalSort {
-    // DFS
+class Solution {
+    // DFS TC-> O(V+E) SC-> O(V)
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i<V; i++) adj.add(new ArrayList<>());
-        for(int[] edge : edges){
-            adj.get(edge[0]).add(edge[1]);
-        }
-        boolean[] visited = new boolean[V];
-        for(int i = 0; i<V; i++){
-            if(visited[i] == false) dfs(i, adj, visited, ans);
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
+        for(int[] edge: edges) adj.get(edge[0]).add(edge[1]);
+        boolean[] visited=new boolean[V];
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            if(visited[i]==false){
+                dfs(adj, i, visited, ans);
+            }
         }
         Collections.reverse(ans);
         return ans;
     }
-    public void dfs(int i, List<List<Integer>> adj, boolean[] visited, List<Integer> list){
-        visited[i] = true;
-        for(int ele : adj.get(i)){
-            if(visited[ele] == false) dfs(ele, adj, visited, list);
+    public void dfs(List<List<Integer>> adj, int i, boolean[] visited, ArrayList<Integer> ans){
+        visited[i]=true;
+        for(int j: adj.get(i)){
+            if(visited[j]==false){
+                dfs(adj, j, visited, ans);
+            }
         }
-        list.add(i);
+        ans.add(i);
     }
-
-    // BFS -> Kahn's Algorithm
-    public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i<V; i++) adj.add(new ArrayList<>());
-        int[] indegree = new int[V];
-        for(int[] edge : edges){
+    
+    // BFS TC-> O(V+E) SC-> O(V) Topological Sort
+    public ArrayList<Integer> topoSort(int V, int[][] edges){
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
+        int[] indegree=new int[V];
+        for(int[] edge: edges){
             adj.get(edge[0]).add(edge[1]);
             indegree[edge[1]]++;
         }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i<V; i++) if(indegree[i] == 0) q.add(i);
-        while(q.size() != 0){
-            int val = q.remove();
+        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.add(i);
+        }
+        while(q.size()!=0){
+            int val=q.remove();
             ans.add(val);
-            for(int ele : adj.get(val)){
-                indegree[ele]--;
-                if(indegree[ele] == 0) q.add(ele);
+            for(int j: adj.get(val)){
+                indegree[j]--;
+                if(indegree[j]==0) q.add(j);
             }
         }
         return ans;
